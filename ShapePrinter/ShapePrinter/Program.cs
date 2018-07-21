fc75e22cd270901323b5940219f721a5b684a61f
Michael Morris <mcmorris@ualberta.ca>
2018-07-20 20:52:46 -0600

    Added solution structure and files

---
+++ ShapePrinter/ShapePrinter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ShapePrinter
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
